8e9c878ebcf5f19c5bc5a4175533f8d4fa0b6aec
productdevbook <hi@productdevbook.com>
2023-10-01 07:14:13 +0300

    fix: test

--- tests/nuxt/purging.test.ts
+++ tests/nuxt/purging.test.ts
@@ -1,14 +1,17 @@
 import { fileURLToPath } from 'node:url'
-import { describe, it } from 'vitest'
-import { setup } from '@nuxt/test-utils'
+import { describe, expect, it } from 'vitest'
+import { setup, useTestContext } from '@nuxt/test-utils'
+import { resolve } from 'path'
+
 
 describe('module', async () => {
   await setup({
-    rootDir: fileURLToPath(new URL('../../playground-nuxt', import.meta.url)),
+    rootDir: resolve(__dirname, '../../playground-nuxt'),
     build: true,
   })
 
   it('should purge classes', async () => {
-    // const ctx = useTestContext()
+    const ctx = useTestContext()
+    expect(true).toBe(true)
   })
 })
